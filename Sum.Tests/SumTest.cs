fb27ebd2b7dc24af3c8cab4862cb2f209ce55464
Slava <dex252rus@gmail.com>
2023-08-26 20:30:53 +1000

    Правки PR

--- Sum.Tests/SumTest.cs
+++ Sum.Tests/SumTest.cs
@@ -1,4 +1,8 @@
+using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+using System.Threading;
 using Xunit;
 
 namespace Sum.Tests
@@ -34,24 +38,36 @@ namespace Sum.Tests
         {
             var stopwatch = new Stopwatch();
             Debug.WriteLine($"Процесс TPLSum для {countOfElements} запущен");
-            const int countOfThreads = 10;
-            var list = GenerateList(countOfElements).ToList();
+            const int countOfThreads = 10; 
             var sum = 0;
+            var list = GenerateList(countOfElements).ToList();
 
-            //Гораздо эффективнее предварительно разбить список на части и найти их суммы по отдельности
-            //Но этот метод помимо подготовки ТД также потребует сложения частей после метода TPL, что противоречит условиям задачи
+            //Подготовка ТД
+            var threads = new Thread[countOfThreads];
+            int partSize = (int)Math.Ceiling((double)list.Count / countOfThreads);
+            for (int index = 0; index < countOfThreads; index++)
+            {
+                var start = index * partSize;
+                var part = list.GetRange(start, Math.Min(partSize, countOfElements - start));
+                var thread = new Thread(() => {
+                    Interlocked.Add(ref sum, part.Sum());
+                });
+                threads[index] = thread;
+            }
 
+            Debug.WriteLine($"Процесс TPLSum для {countOfElements} запущен");
             stopwatch.Start();
-            Parallel.For(0, countOfElements, new ParallelOptions()
-            {
-                MaxDegreeOfParallelism = countOfThreads
-            }, (index)=>
-            {
-                Interlocked.Add(ref sum, list[index]);
+
+            Parallel.ForEach(threads, thread => {
+                thread.Start();
             });
 
-            stopwatch.Stop();
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
 
+            stopwatch.Stop();
             var elapsedTime = stopwatch.Elapsed.TotalMilliseconds;
             Debug.WriteLine($"Процесс TPLSum для {countOfElements} элементов завершен за {elapsedTime} ms, сумма = {sum}");
 
@@ -89,9 +105,9 @@ namespace Sum.Tests
         /// </summary>
         /// <param name="countOfElements"></param>
         [Theory]
-        [InlineData(100_000)]
-        [InlineData(1_000_000)]
-        [InlineData(10_000_000)]
+    //    [InlineData(100_000)]
+    // [InlineData(1_000_000)]
+       [InlineData(10_000_000)]
         public void Common(int countOfElements)
         {
             var stopwatch = new Stopwatch();
@@ -109,15 +125,30 @@ namespace Sum.Tests
             stopwatch.Reset();
 
             sum = 0;
+            //Подготовка ТД
+            var threads = new Thread[countOfThreads];
+            int partSize = (int)Math.Ceiling((double)list.Count / countOfThreads);
+            for (int index = 0; index < countOfThreads; index++)
+            {
+                var start = index * partSize;
+                var part = list.GetRange(start, Math.Min(partSize, countOfElements - start));
+                var thread = new Thread(() => {
+                    Interlocked.Add(ref sum, part.Sum());
+                });
+                threads[index] = thread;
+            }
+
             Debug.WriteLine($"Процесс TPLSum для {countOfElements} запущен");
             stopwatch.Start();
-            Parallel.For(0, countOfElements, new ParallelOptions()
-            {
-                MaxDegreeOfParallelism = countOfThreads
-            }, (index) =>
-            {
-                Interlocked.Add(ref sum, list[index]);
+
+            Parallel.ForEach(threads, thread => {
+                thread.Start(); 
             });
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
 
             stopwatch.Stop();
 
@@ -125,7 +156,7 @@ namespace Sum.Tests
             Debug.WriteLine($"Процесс TPLSum для {countOfElements} элементов завершен за {elapsedTime} ms, сумма = {sum}");
             
             stopwatch.Reset();
-
+            threads = null;
 
             sum = 0;
             Debug.WriteLine($"Процесс LinqSum для {countOfElements} запущен");
